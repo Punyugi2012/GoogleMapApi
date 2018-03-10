5d1797fe21ebd91fe4d1feb90948d7e0dff44449
Punyugi2012 <58160216@go.buu.ac.th>
2018-03-10 19:18:43 +0700

    เปลี่ยนเป็น array of json

--- index.php
+++ index.php
@@ -21,38 +21,40 @@
   <body>
     <div id="map"></div>
     <script>
-      var maps;
+      var map;
       function initMap() {
         var beaches = [
-          ['Bondi Beach', -33.890542, 151.274856],
-          ['Coogee Beach', -33.923036, 151.259052],
-          ['Cronulla Beach', -34.028249, 151.157507],
-          ['Manly Beach', -33.80010128657071, 151.28747820854187],
-          ['Maroubra Beach', -33.950198, 151.259302]
+          {'location':'Bondi Beach', 'lat':-33.890542, 'lng':151.274856},
+          {'location':'Coogee Beach', 'lat':-33.923036, 'lng':151.259052},
+          {'location':'Cronulla Beach', 'lat':-34.028249, 'lng':151.157507},
+          {'location':'Manly Beach', 'lat':-33.80010128657071, 'lng':151.28747820854187},
+          {'location':'Maroubra Beach', 'lat':-33.950198, 'lng':151.259302}
         ];
         map = new google.maps.Map(document.getElementById('map'), {
-          center: new google.maps.LatLng(beaches[0][1], beaches[0][2]),
+          center: new google.maps.LatLng(beaches[0].lat, beaches[0].lng),
           zoom: 10,
           mapTypeId: google.maps.MapTypeId.TERRAIN
         });
 
-        var marker, info;
-        var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+        var marker, info, image;
+        image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
 
-        for(var i = 0; i < beaches.length; i++) {
+        beaches.forEach(function(beach, index) {
           marker = new google.maps.Marker({
-            position: new google.maps.LatLng(beaches[i][1], beaches[i][2]),
+            position: new google.maps.LatLng(beach.lat, beach.lng),
             map: map,
             icon: image
           });
+
           info = new google.maps.InfoWindow();
-          google.maps.event.addListener(marker, 'click', (function(marker, i) {
+          google.maps.event.addListener(marker, 'click', (function(marker, beach) {
             return function() {
-              info.setContent(beaches[i][0]);
+              info.setContent(beach.location);
               info.open(map, marker)
             }
-          })(marker, i));
-        }
+          })(marker, beach));
+        })
+      
         
       }
     </script>
